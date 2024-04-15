215d91975e87b10910b1fe065f6cd169f9173821
JamalArsh <92667028+JamalArsh@users.noreply.github.com>
2024-04-15 10:05:59 +0530

    initial commit

---
+++ LeetCode/1-TwoSum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+// learned: define variables for correct scope
+// todo: reduce time complexity
+
+var twoSum = function (nums, target) {
+  let output;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        output = [i, j];
+      }
+    }
+  }
+  return output;
+};
